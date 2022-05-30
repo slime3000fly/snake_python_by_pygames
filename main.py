448c1beb7f86698636ea6494185e761faa52405c
slime3000fly <94618871+slime3000fly@users.noreply.github.com>
2022-05-30 19:59:50 +0200

    funckja streowania szwawnkuje

--- main.py
+++ main.py
@@ -1,19 +1,6 @@
 import pygame, sys, time, random
 
 
-def touch():
-    # function check if snake is colliding itself
-    for i in range(1, size):
-        for k in range(0, 11):
-            if (snake_head_x == x[i] + k):
-                for k in range(0, 11):
-                    if (snake_head_y == y[i] + k):
-                        lose = 1
-                        print(lose)
-                        print('gowno')
-                        return lose
-
-
 pygame.init()
 pygame.display.init()
 fps_controller = pygame.time.Clock()
@@ -21,16 +8,77 @@ fps_controller = pygame.time.Clock()
 clock = pygame.time.Clock()
 screen = pygame.display.set_mode((1080, 720))
 
+#variable declartaion
 x = [200]
 y = [200]
 checker = [0]
-z1 = 0
-z2 = 0
-move = 20  # variable which define how big is single step for snake
-lose = 0
 number_of_apple = 0
 size = 1  # variable which define how long is snake
 t = 0.5
+
+# function declaration
+def lose():
+    # function which play after lose game
+    while 1:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT: sys.exit()
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_ESCAPE:
+                    lose = 0
+        # drawing writing 'you lose'
+        font = pygame.font.Font('freesansbold.ttf', 52)
+        text = font.render('YOU LOSE', True, red, white)
+        # text surface object
+        textRect = text.get_rect()
+
+        # set the center of the rectangular object.
+        textRect.center = (1080 // 2, 720 // 2)
+
+        screen.blit(text, textRect)
+
+        pygame.display.update()
+def touch():
+    #function to check if snake touch itself or wall
+    snake_head_x = x[0]
+    snake_head_y = y[0]
+    # check if snake is colliding itself
+    for i in range(1, size):
+        for k in range(0, 11):
+            if (snake_head_x == x[i] + k):
+                for k in range(0, 11):
+                    if (snake_head_y == y[i] + k):
+                        losek = 1
+                        print(losek)
+                        print('gowno')
+                        lose()
+    # check if snake is touching wall
+    if snake_head_x >= 1040 or snake_head_x <= 10 or snake_head_y >= 700 or snake_head_y <= 10: lose()
+def control():
+    #function to control snake
+    z1 = 0
+    z2 = 0
+    move = 20  # variable which define how big is single step for snake
+    # key to control snake
+    if event.type == pygame.KEYDOWN:
+        if event.key == pygame.K_UP:
+            z1 = - move
+            z2 = 0
+        if event.key == pygame.K_DOWN:
+            z1 = move
+            z2 = 0
+        if event.key == pygame.K_RIGHT:
+            z2 = move
+            z1 = 0
+        if event.key == pygame.K_LEFT:
+            z2 = - move
+            z1 = 0
+        if event.key == pygame.K_ESCAPE:
+            done = True
+
+    # nubmer to move snake
+    x[0] = x[0] + z2
+    y[0] = y[0] + z1
+    return x[0],y[0]
 
 # color
 red = pygame.Color(139, 0, 0)
@@ -46,123 +94,74 @@ done = False
 
 while not done:
 
-    while lose == 0:
-        # print(size)
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT: sys.exit()
-
-        # adding new element to list x and y
-        for i in range(0, size):
-            if (i >= len(x)):
-                x.append(i)
-                y.append(i)
-
-        if x[0] >= 1040 or x[0] <= 10 or y[0] >= 700 or y[0] <= 10: lose = 1  # check if snake touch wall
-        # check if snake is touching itself
-        snake_head_x = x[0]
-        snake_head_y = y[0]
-
-        touch()
-
-        # key to control snake
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_UP:
-                z1 = - move
-                z2 = 0
-            if event.key == pygame.K_DOWN:
-                z1 = move
-                z2 = 0
-            if event.key == pygame.K_RIGHT:
-                z2 = move
-                z1 = 0
-            if event.key == pygame.K_LEFT:
-                z2 = - move
-                z1 = 0
-            if event.key == pygame.K_ESCAPE:
-                done = True
-                lose = 1
-
-        # nubmer to move snake
-        x[0] = x[0] + z2
-        y[0] = y[0] + z1
-
-        # create x and y for drawing apple
-        if number_of_apple == 0:
-            x_apple = random.randint(20, 1060)
-            y_apple = random.randint(20, 700)
-            number_of_apple = 1
-
-        screen.fill(white)
-        # drawing snake
-        i = 0
-        for i in range(0, size):
-            # drawing snake head
-            if (i == 0): snake_head = pygame.draw.rect(screen, yellow, pygame.Rect(x[i], y[i], 20, 20))
-            # drawing another part of snake
-            if (i == 1):
-                temp_x2 = x[i]
-                temp_y2 = y[i]
-                x[i] = previos_x
-                y[i] = previos_y
-                pygame.draw.rect(screen, blue, pygame.Rect(x[i], y[i], 20, 20))
-            if (i >= 2):
-                if (i % 2 == 0):
-                    temp_x1 = x[i]
-                    x[i] = temp_x2
-                    temp_y1 = y[i]
-                    y[i] = temp_y2
-                    pygame.draw.rect(screen, blue, pygame.Rect(x[i], y[i], 20, 20))
-                else:
-                    temp_x2 = x[i]
-                    x[i] = temp_x1
-                    temp_y2 = y[i]
-                    y[i] = temp_y1
-                    pygame.draw.rect(screen, blue, pygame.Rect(x[i], y[i], 20, 20))
-
-        # draw apple
-        pygame.draw.rect(screen, red, pygame.Rect(x_apple, y_apple, 10, 10))
-
-        # drawing wall
-        pygame.draw.rect(screen, orange, pygame.Rect(0, 0, 10, 720))
-        pygame.draw.rect(screen, orange, pygame.Rect(1070, 0, 10, 720))
-        pygame.draw.rect(screen, orange, pygame.Rect(0, 0, 1080, 10))
-        pygame.draw.rect(screen, orange, pygame.Rect(0, 710, 1080, 10))
-
-        pygame.display.flip()
-
-        # check if snake eat apple
-        i = 0
-        for i in range(-21, 21):
-            if (x[0] + i == x_apple):
-                for i in range(-21, 21):
-                    if (y[0] + i == y_apple):
-                        number_of_apple = 0
-                        size += 10
-
-        # FPS !!!!!
-        fps_controller.tick(12)
-
-        previos_x = x[0]
-        previos_y = y[0]
-
-    # #after lose game
+    # print(size)
     for event in pygame.event.get():
         if event.type == pygame.QUIT: sys.exit()
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_ESCAPE:
-                lose = 0
-    # drawing writing 'you lose'
-    font = pygame.font.Font('freesansbold.ttf', 52)
-    text = font.render('YOU LOSE', True, red, white)
-    # text surface object
-    textRect = text.get_rect()
 
-    # set the center of the rectangular object.
-    textRect.center = (1080 // 2, 720 // 2)
+    # adding new element to list x and y
+    for i in range(0, size):
+        if (i >= len(x)):
+            x.append(i)
+            y.append(i)
 
-    screen.blit(text, textRect)
+    touch()
+    control()
 
-    pygame.display.update()
+    # create x and y for drawing apple
+    if number_of_apple == 0:
+        x_apple = random.randint(20, 1060)
+        y_apple = random.randint(20, 700)
+        number_of_apple = 1
 
+    screen.fill(white)
+    # drawing snake
+    i = 0
+    for i in range(0, size):
+        # drawing snake head
+        if (i == 0): snake_head = pygame.draw.rect(screen, yellow, pygame.Rect(x[i], y[i], 20, 20))
+        # drawing another part of snake
+        if (i == 1):
+            temp_x2 = x[i]
+            temp_y2 = y[i]
+            x[i] = previos_x
+            y[i] = previos_y
+            pygame.draw.rect(screen, blue, pygame.Rect(x[i], y[i], 20, 20))
+        if (i >= 2):
+            if (i % 2 == 0):
+                temp_x1 = x[i]
+                x[i] = temp_x2
+                temp_y1 = y[i]
+                y[i] = temp_y2
+                pygame.draw.rect(screen, blue, pygame.Rect(x[i], y[i], 20, 20))
+            else:
+                temp_x2 = x[i]
+                x[i] = temp_x1
+                temp_y2 = y[i]
+                y[i] = temp_y1
+                pygame.draw.rect(screen, blue, pygame.Rect(x[i], y[i], 20, 20))
 
+    # draw apple
+    pygame.draw.rect(screen, red, pygame.Rect(x_apple, y_apple, 10, 10))
 
+    # drawing wall
+    pygame.draw.rect(screen, orange, pygame.Rect(0, 0, 10, 720))
+    pygame.draw.rect(screen, orange, pygame.Rect(1070, 0, 10, 720))
+    pygame.draw.rect(screen, orange, pygame.Rect(0, 0, 1080, 10))
+    pygame.draw.rect(screen, orange, pygame.Rect(0, 710, 1080, 10))
+
+    pygame.display.flip()
+
+    # check if snake eat apple
+    i = 0
+    for i in range(-21, 21):
+        if (x[0] + i == x_apple):
+            for i in range(-21, 21):
+                if (y[0] + i == y_apple):
+                    number_of_apple = 0
+                    size += 10
+
+    # FPS !!!!!
+    fps_controller.tick(12)
+
+    previos_x = x[0]
+    previos_y = y[0]
